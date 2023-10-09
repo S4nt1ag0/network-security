f04293f0bfbc935dc273465f3c7a4413d7e2458c
S4nt1ag0 <gustavosantiago1018@gmail.com>
2023-10-09 13:22:54 -0300

    improve RSA description

--- RSACipher.py
+++ RSACipher.py
@@ -6,20 +6,22 @@ def mdc(a, b):
     else:
         return mdc(b, a % b)
 
-def mod_inverso(a, m):
+def mod_inverse(a, m):
     for x in range(1, m):
         if (a * x) % m == 1:
             return x
     return -1
 
-def geraParChaves(keysize = 16):
+def generateKeyPair(keysize = 16):
+    #configurando tamanho da chave, valores maximos, minimos e de onde começa e termina a geração.
     nMin = 1 << (keysize - 1)
     nMax = (1 << keysize) - 1
     primos = [2]
-    ini = 1 << (keysize // 2 - 1)
-    fim = 1 << (keysize // 2 + 1)
+    start = 1 << (keysize // 2 - 1)
+    end = 1 << (keysize // 2 + 1)
 
-    for i in range(3, fim + 1, 2):
+    #Selecionando todos os primos entre start e end.
+    for i in range(3, end + 1, 2):
         isPrime = True
         for p in primos:
             if i % p == 0:
@@ -28,8 +30,11 @@ def geraParChaves(keysize = 16):
         if(isPrime):
             primos.append(i)
 
-    while (primos and primos[0] < ini):
+    #Excluindo os primos menores que o valor inicial
+    while (primos and primos[0] < start):
         del primos[0]
+
+    #Dos primos restantes, seleciona um aleatorio para ser p, e outro para ser q, desde que p*q não seja maior que nMax
     while primos:
         p = random.choice(primos)
         primos.remove(p)
@@ -38,15 +43,16 @@ def geraParChaves(keysize = 16):
             q = random.choice(q_values)
             break
 
-    print(p, q)
+    print("p e q:", p, q)
     n = p * q
     phi = (p - 1) * (q - 1)
 
+    #gera a chave publica e privada desde que mdc(e, phi) == 1 e 'e' != d
     while True:
         e = random.randrange(1, phi)
         g = mdc(e, phi)
         # gera chave privada
-        d = mod_inverso(e, phi)
+        d = mod_inverse(e, phi)
         if g == 1 and e != d:
             break
     # chave pública (e,n)
@@ -55,18 +61,21 @@ def geraParChaves(keysize = 16):
 
 def criptografa(msg_original, chave_valor):
     e, n = chave_valor
+    #para cada caractere da mensagem original, seu valor na tabela ascii é elavado a 'e' e calculado o mod n
     msg_cifrada = [pow(ord(c), e, n) for c in msg_original]
     return msg_cifrada
 
 def descriptografa(msg_cifrada, chave_valor):
     d, n = chave_valor
+    #Cada número da mensagem criptografada é elavado a d e calculado o mod n, então se extrai o caractere correspondente
+    #a esse valor na tabela ascii
     msg_original = [chr(pow(c, d, n)) for c in msg_cifrada]
     return (''.join(msg_original))
 
 # -------------------------------------------------------------
 print("Iniciando algoritmo RSA...")
 print("Gerando par de chaves privadas/públicas...")
-publica, privada = geraParChaves(16)
+publica, privada = generateKeyPair()
 print("Chave pública:", publica)
 print("Chave privada:", privada)
 msg = input("Mensagem que deseja criptografar:")
